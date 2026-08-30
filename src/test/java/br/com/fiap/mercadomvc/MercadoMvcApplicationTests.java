7c66f985f0a48abf94c56818f063950766ea8fcc
GusCrevelari <gustavocrevelari@hotmail.com>
2026-08-30 19:32:35 -0300

    project initialization;

---
+++ src/test/java/br/com/fiap/mercadomvc/MercadoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mercadomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
